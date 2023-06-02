495ed8940287082e6e856b3e13ac1df3b833a490
Nishthakumari <knishtha0@gmail.com>
2023-06-02 19:13:58 +0530

    @PostMapping | Creating Resource REST API | REST API using Spring Boot | Spring Boot Tutorial

--- restapi/src/main/java/com/nishtha/restapi/controller/BookController.java
+++ restapi/src/main/java/com/nishtha/restapi/controller/BookController.java
@@ -3,9 +3,11 @@ package com.nishtha.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nishtha.restapi.entities.Book;
@@ -26,5 +28,11 @@ public class BookController {
     public Book getBook(@PathVariable("id") int id) {
 
         return this.bookService.getBookById(id);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book b) {
+        Book book = this.bookService.addBook(b);
+        return book;
     }
 }
--- restapi/src/main/java/com/nishtha/restapi/services/BookService.java
+++ restapi/src/main/java/com/nishtha/restapi/services/BookService.java
@@ -28,4 +28,9 @@ public class BookService {
         book = list.stream().filter(e -> e.getId() == id).findFirst().get();
         return book;
     }
+
+    public Book addBook(Book b) {
+        list.add(b);
+        return b;
+    }
 }
