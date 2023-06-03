e640021337afb64b9eff268e3dec75decf6484c9
Nishthakumari <knishtha0@gmail.com>
2023-06-03 16:22:31 +0530

    Image Uploading to dynamic path | Serving image file

--- restapi/src/main/java/com/nishtha/restapi/helper/FileUploadHelper.java
+++ restapi/src/main/java/com/nishtha/restapi/helper/FileUploadHelper.java
@@ -6,13 +6,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-    public String UPLOAD_DIR = "C:\\Users\\Nishtha\\Documents\\Projects\\restapi\\src\\main\\resources\\static\\image";
+    // public String UPLOAD_DIR =
+    // "C:\\Users\\Nishtha\\Documents\\Projects\\restapi\\src\\main\\resources\\static\\image";
+    public String UPLOAD_DIR = new ClassPathResource("static/image/").getFile().getAbsolutePath();
+
+    public FileUploadHelper() throws IOException {
+
+    }
 
     public boolean uploadFile(MultipartFile multipartFile) {
 
